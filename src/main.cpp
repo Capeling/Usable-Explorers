d3bd19e9197db72ae6d05c496d2fb2025c13a223
Capeling <113705504+Capeling@users.noreply.github.com>
2024-01-24 15:52:46 +1030

    removed base::get(), see if that does anythjing

--- src/main.cpp
+++ src/main.cpp
@@ -24,12 +24,12 @@ class $modify(MenuLayer) {
 
 		#ifdef GEODE_IS_ANDROID64
 		log::info("patching ANDROID64");
-		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x50b34c), { 0x3f, 0x58, 0x00, 0x71 });
-		log::info("patched {}", base::get() + 0x50b34c);
-		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x50b328), { 0xc3, 0x02, 0x80, 0x52 });
-		log::info("patched {}", base::get() + 0x50b328);
-		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x50b324), { 0x3f, 0x58, 0x00, 0x71 });
-		log::info("patched {}", base::get() + 0x50b324);
+		Mod::get()->patch(reinterpret_cast<void*>(0x50b34c), { 0x3f, 0x58, 0x00, 0x71 });
+		log::info("patched {}", 0x50b34c);
+		Mod::get()->patch(reinterpret_cast<void*>(0x50b328), { 0xc3, 0x02, 0x80, 0x52 });
+		log::info("patched {}", 0x50b328);
+		Mod::get()->patch(reinterpret_cast<void*>(0x50b324), { 0x3f, 0x58, 0x00, 0x71 });
+		log::info("patched {}", 0x50b324);
 		#endif
 
 		return true;
