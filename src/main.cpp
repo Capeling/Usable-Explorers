b7fc3eee835454379efca96795644d21bebee693
Capeling <113705504+Capeling@users.noreply.github.com>
2024-01-24 15:18:22 +1030

    Android64 Support

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,23 @@ class $modify(MenuLayer) {
 		if(!MenuLayer::init())
 			return false;
 		#ifdef GEODE_IS_WINDOWS
+		log::info("patching WINDOWS");
 		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x92276), { 0x83, 0xf8, 0x16 });
 		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x92283), { 0xba, 0x16, 0x00, 0x00, 0x00 });
 		#endif
 
-		#ifdef GEODE_IS_ANDROID
+		#ifdef GEODE_IS_ANDROID32
+		log::info("patching ANDROID32");
 		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x5241e), { 0x16, 0x29 });
 		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x52400), { 0x16, 0x29 });
-		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x5240c), { 0x16, 0x21 });
+		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x5240c), { 0x15, 0x21 });
+		#endif
+
+		#ifdef GEODE_IS_ANDROID64
+		log::info("patching ANDROID64");
+		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x50b34c), { 0x3f, 0x58, 0x00, 0x71 });
+		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x50b328), { 0xc3, 0x02, 0x80, 0x52 });
+		Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x50b324), { 0x3f, 0x58, 0x00, 0x71 });
 		#endif
 
 		return true;
